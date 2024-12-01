aecaafba0e1168363017690fd0a5a598514abcbc
Glêsio Santos <glesioss@gmail.com>
2024-12-01 09:19:11 -0300

    add formulario para cadastro de produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ProductListComponent } from './features/product/product-list/product-list.component';
-import { Observable } from 'rxjs';
-import { inject } from '@angular/core';
-import { ProductService } from './shared/services/product.service';
 
 export const routes: Routes = [
   {path:'', component: ProductListComponent},
@@ -11,14 +8,14 @@ export const routes: Routes = [
       m => m.CreateProductComponent
     ), // mudando o carregamento para lazy load para aumentar a performace
   },
-  {path:'edit/:id',
-    resolve: {
-      product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        const productService = inject(ProductService);
-        return productService.get(route.paramMap.get('id') as string);
-      }
-    },
-    loadComponent: () => import('./features/product/product-edit/product-edit.component').then(
-      m => m.ProductEditComponent)
-  }
+  // {path:'edit/:id',
+  //   resolve: {
+  //     product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  //       const productService = inject(ProductService);
+  //       return productService.get(route.paramMap.get('id') as string);
+  //     }
+  //   },
+  //   loadComponent: () => import('./features/product/product-edit/product-edit.component').then(
+  //     m => m.ProductEditComponent)
+  // }
 ];
--- src/app/features/product/product-list/product-list.component.ts
+++ src/app/features/product/product-list/product-list.component.ts
@@ -3,13 +3,13 @@ import { ProductService } from '../../../shared/services/product.service';
 import { Product } from '../../../shared/models/product.model';
 import { CardComponent } from '../components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
-import { MatButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButton],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
